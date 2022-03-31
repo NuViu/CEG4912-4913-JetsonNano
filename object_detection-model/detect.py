ecae5a5c6915db0e7c692887bf4e159b935dd9aa
samarthks <samarthk0105@gmail.com>
2022-03-31 01:41:36 -0400

    object detection model on video working

---
+++ object_detection-model/detect.py
@@ -0,0 +1,20 @@
+from imageai.Detection import VideoObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = VideoObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
+detector.loadModel()
+
+#will run for 5 seconds
+video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "traffic.mp4"),
+                                output_file_path=os.path.join(execution_path,"traffic_detection")
+                                , frames_per_second=20, log_progress=True,detection_timeout=5) 
+
+
+
+print(video_path)
+
+
