a10df7af507d1083778c9785c0e3869776f9f7c1
Zain <47285447+ZainMalik00@users.noreply.github.com>
2022-04-07 11:03:41 -0400

    Added Unit Tests

--- object_detection-model/detect.py
+++ object_detection-model/detect.py
@@ -6,29 +6,29 @@ import pandas
 execution_path = os.getcwd()
 
 detector = VideoObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath(os.path.join(execution_path, "resnet50_coco_best_v2.1.0.h5"))
+detector.setModelTypeAsTinyYOLOv3()
+detector.setModelPath(os.path.join(execution_path, "./object_detection-model/yolo-tiny.h5"))
 detector.loadModel()
 
 
 def forFrame(frame_number, output_array, output_count):
-    print("FOR FRAME ", frame_number)
+    #print("FOR FRAME ", frame_number)
     keys = output_array[0].keys()
 
-    with open("data.csv", "w", newline="") as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(output_array)
+    # with open("data.csv", "w", newline="") as output_file:
+    #     dict_writer = csv.DictWriter(output_file, keys)
+    #     dict_writer.writeheader()
+    #     dict_writer.writerows(output_array)
 
     # print("Output for each object : ",output_array)
-    print("Output count for unique objects : ", output_count)
-    print("------------END OF A FRAME --------------")
+    #print("Output count for unique objects : ", output_count)
+    #print("------------END OF A FRAME --------------")
 
 
 # will run for 5 seconds
 video_path = detector.detectObjectsFromVideo(
-    input_file_path=os.path.join(execution_path, "./train_video/trafficsign.mp4"),
-    output_file_path=os.path.join(execution_path, "./output_video/traffic_detections"),
+    input_file_path=os.path.join(execution_path, "./object_detection-model/train_video/walk_downtown_la.mp4"),
+    output_file_path=os.path.join(execution_path, "./object_detection-model/output_video/walk_downtown_la_detection_yolo"),
     frames_per_second=20,
     per_frame_function=forFrame,
     log_progress=True,
--- object_detection-model/detect_camera.py
+++ object_detection-model/detect_camera.py
@@ -7,29 +7,31 @@ execution_path = os.getcwd()
 
 
 camera = cv2.VideoCapture(0)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH,256);
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT,256);
 
 detector = VideoObjectDetection()
 detector.setModelTypeAsRetinaNet()
-detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
+detector.setModelPath(os.path.join(execution_path , "./object_detection-model/resnet50_coco_best_v2.1.0.h5"))
 detector.loadModel()
 
-def forFrame(frame_number, output_array, output_count):
-    print("FOR FRAME ", frame_number)
-    keys = output_array[0].keys()
+# def forFrame(frame_number, output_array, output_count):
+    # print("FOR FRAME ", frame_number)
+    # keys = output_array[0].keys()
 
-    with open("data_cam.csv", "w", newline="") as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(output_array)
+    # with open("data_cam.csv", "w", newline="") as output_file:
+    #     dict_writer = csv.DictWriter(output_file, keys)
+    #     dict_writer.writeheader()
+    #     dict_writer.writerows(output_array)
 
-    # print("Output for each object : ",output_array)
-    print("Output count for unique objects : ", output_count)
-    print("------------END OF A FRAME --------------")
+    # # print("Output for each object : ",output_array)
+    # print("Output count for unique objects : ", output_count)
+    # print("------------END OF A FRAME --------------")
 
 video_path = detector.detectObjectsFromVideo(
                 camera_input=camera,
-                output_file_path=os.path.join(execution_path, "./output_video/camera_detect"),
-                frames_per_second=20,  per_frame_function=forFrame,log_progress=True, minimum_percentage_probability=40,detection_timeout=1)
+                output_file_path=os.path.join(execution_path, "./object_detection-model/output_video/camera_detect_test"),
+                frames_per_second=20, log_progress=True, minimum_percentage_probability=40,detection_timeout=1)
 
 
 print(video_path)
